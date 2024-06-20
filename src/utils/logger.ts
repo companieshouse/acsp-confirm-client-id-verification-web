718a494c6093af9ea42d3cb72495c512bf79ae2e
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-06-20 16:50:18 +0100

    req changes

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,10 @@
 import { createLogger } from "@companieshouse/structured-logging-node";
- import ApplicationLogger from "@companieshouse/structured-logging-node/lib/ApplicationLogger";
+import ApplicationLogger from "@companieshouse/structured-logging-node/lib/ApplicationLogger";
 
- export const logger: ApplicationLogger = createLogger("acsp-confirm-client-id-verification-web");
+export const logger: ApplicationLogger = createLogger("acsp-confirm-client-id-verification-web");
 
- export const createAndLogError = (description: string): Error => {
-     const error = new Error(description);
-     logger.error(`${error.stack}`);
-     return error;
- };
+export const createAndLogError = (description: string): Error => {
+    const error = new Error(description);
+    logger.error(`${error.stack}`);
+    return error;
+};
