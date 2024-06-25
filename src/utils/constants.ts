12c6ace8b302af2f5db7133c957dd8e0a4e9e9b2
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-06-25 12:27:18 +0100

    sonar fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,2 @@
 export const USER_DATA = "user";
 export const PREVIOUS_PAGE_URL: string = "previouspageurl";
-export const ERROR_MESSAGE: String = "Sorry we are experiencing technical difficulties";
