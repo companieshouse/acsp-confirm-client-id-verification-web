0bd5684be1199ea5a0b99cadca419c6363a8f5c1
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-07-23 14:51:38 +0100

    -updated validation conditions

--- src/validations/identityDocumentsGroup2.ts
+++ src/validations/identityDocumentsGroup2.ts
@@ -1,6 +1,43 @@
-import { body } from "express-validator";
+import { body, ValidationChain } from 'express-validator';
 
-export const identityDocsGroup2Validator = [
-    body("documentsGroup2A", "identityDocumentsGroup1Empty").notEmpty(),
-    body("documentsGroup2B", "identityDocumentsGroup1Empty").notEmpty()
+export const identityDocsGroup2Validator: ValidationChain[] = [
+    body('documentsGroup2A')
+        .custom((value, { req }) => {
+
+
+            let documentsGroup2A: any[] = [];
+            let documentsGroup2B: any[] = [];
+
+            if (req.body.documentsGroup2A === undefined) {
+                documentsGroup2A = [];
+            } else if (Array.isArray(req.body.documentsGroup2A)) {
+                documentsGroup2A = req.body.documentsGroup2A;
+            } else {
+                documentsGroup2A = [req.body.documentsGroup2A];
+            }
+
+            if (req.body.documentsGroup2B === undefined) {
+                documentsGroup2B = [];
+            } else if (Array.isArray(req.body.documentsGroup2B)) {
+                documentsGroup2B = req.body.documentsGroup2B;
+            } else {
+                documentsGroup2B = [req.body.documentsGroup2B];
+            }
+
+
+
+            if (documentsGroup2A.length === 0 && documentsGroup2B.length === 0) {
+                throw new Error("identityDocumentsGroup1Empty");
+
+            }
+            if (documentsGroup2B.length !== 0 && documentsGroup2A.length === 0) {
+                throw new Error("identityDocumentsGroupANotSelected");
+            }
+            if ((documentsGroup2B.length + documentsGroup2A.length) < 2) {
+                throw new Error("identityDocumentsNo2Selection");
+            }
+            return true
+
+        }),
+
 ];
