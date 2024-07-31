89a5a6efd7a05c94b76d2c3b22c5389dd4632860
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-07-31 16:30:49 +0100

    -added checkanswer page

--- src/controllers/checkYourAnswers.ts
+++ src/controllers/checkYourAnswers.ts
@@ -5,18 +5,24 @@ import { BASE_URL, CHECK_YOUR_ANSWERS, CONFIRMATION } from "../types/pageURL";
 import { USER_DATA } from "../utils/constants";
 import { ClientData } from "../model/ClientData";
 import { Session } from "@companieshouse/node-session-handler";
-
+//import { ClientDataService } from "../services/checkYourAnswer";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
     const locales = getLocalesService();
     const session: Session = req.session as any as Session;
     const clientData: ClientData = session.getExtraData(USER_DATA) ? session.getExtraData(USER_DATA)! : {};
-    
+    //const clientDataService = new ClientDataService();
+    //clientDataService.saveDataInSession(req);
+
+    console.log(USER_DATA);
+
     res.render(config.CHECK_YOUR_ANSWERS, {
         title: "Check your answers before sending your application",
         ...getLocaleInfo(locales, lang),
-        currentUrl: BASE_URL + CHECK_YOUR_ANSWERS
+        currentUrl: BASE_URL + CHECK_YOUR_ANSWERS,
+        firstName: clientData?.firstName,
+        lastName: clientData?.lastName
     });
 };
 
---
+++ src/services/checkYourAnswer.ts
@@ -0,0 +1,8 @@
+
+import { Request } from "express";
+import { ClientData } from "model/ClientData";
+import { Session } from "@companieshouse/node-session-handler";
+import { USER_DATA } from "../utils/constants";
+import { saveDataInSession } from "utils/sessionHelper";
+
+
