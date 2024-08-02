ffd17999e1ab691a97e251984e6a54230b779100
abhiahirv1 <aahir@companieshouse.gov.uk>
2024-08-02 14:36:33 +0100

    Lint fix

--- src/controllers/confirmationController.ts
+++ src/controllers/confirmationController.ts
@@ -1,13 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import * as config from "../config";
-import { FormatService } from '../services/formatService';
+import { FormatService } from "../services/formatService";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../utils/localise";
 import { CHECK_YOUR_ANSWERS, BASE_URL, CONFIRMATION } from "../types/pageURL";
 import { Session } from "@companieshouse/node-session-handler";
 import { USER_DATA } from "../utils/constants";
 import { ClientData } from "../model/ClientData";
-
-
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     const lang = selectLang(req.query.lang);
@@ -15,23 +13,23 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     const session: Session = req.session as any as Session;
     const clientData: ClientData = session.getExtraData(USER_DATA) ? session.getExtraData(USER_DATA)! : {};
 
-  const formattedAddress = FormatService.formatAddress(clientData.address);
-  const formattedDateOfBirth = FormatService.formatDate(clientData.dateOfBirth ? new Date(clientData.dateOfBirth) : undefined);
-  const formattedwhenIdentityChecksCompleted = FormatService.formatDate(clientData.whenIdentityChecksCompleted ? new Date(clientData.whenIdentityChecksCompleted) : undefined);
-  const formattedDocumentsChecked = FormatService.formatDocumentsChecked(clientData.documentsChecked, lang);
-  const formattedHowIdentityDocsChecked = FormatService.formatHowIdentityDocsChecked(clientData.howIdentityDocsChecked, lang);
+    const formattedAddress = FormatService.formatAddress(clientData.address);
+    const formattedDateOfBirth = FormatService.formatDate(clientData.dateOfBirth ? new Date(clientData.dateOfBirth) : undefined);
+    const formattedwhenIdentityChecksCompleted = FormatService.formatDate(clientData.whenIdentityChecksCompleted ? new Date(clientData.whenIdentityChecksCompleted) : undefined);
+    const formattedDocumentsChecked = FormatService.formatDocumentsChecked(clientData.documentsChecked, lang);
+    const formattedHowIdentityDocsChecked = FormatService.formatHowIdentityDocsChecked(clientData.howIdentityDocsChecked, lang);
 
-  res.render(config.CONFIRMATION, {
-    previousPage: addLangToUrl(BASE_URL + CHECK_YOUR_ANSWERS, lang),
-    ...getLocaleInfo(locales, lang),
-    currentUrl: BASE_URL + CONFIRMATION,
-    clientData: {
-      ...clientData,
-      address: formattedAddress,
-      dateOfBirth: formattedDateOfBirth,
-      whenIdentityChecksCompleted: formattedwhenIdentityChecksCompleted,
-      documentsChecked: formattedDocumentsChecked,
-      howIdentityDocsChecked: formattedHowIdentityDocsChecked
-    }
-  });
+    res.render(config.CONFIRMATION, {
+        previousPage: addLangToUrl(BASE_URL + CHECK_YOUR_ANSWERS, lang),
+        ...getLocaleInfo(locales, lang),
+        currentUrl: BASE_URL + CONFIRMATION,
+        clientData: {
+            ...clientData,
+            address: formattedAddress,
+            dateOfBirth: formattedDateOfBirth,
+            whenIdentityChecksCompleted: formattedwhenIdentityChecksCompleted,
+            documentsChecked: formattedDocumentsChecked,
+            howIdentityDocsChecked: formattedHowIdentityDocsChecked
+        }
+    });
 };
