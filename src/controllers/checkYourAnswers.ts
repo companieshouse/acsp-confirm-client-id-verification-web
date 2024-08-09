77245123c4d773dadaa9c5f5404287cbdeb34cf2
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-08-09 16:36:37 +0100

    update page title, back button

--- src/controllers/checkYourAnswers.ts
+++ src/controllers/checkYourAnswers.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { selectLang, addLangToUrl, getLocalesService, getLocaleInfo } from "../utils/localise";
 import * as config from "../config";
-import { BASE_URL, CHECK_YOUR_ANSWERS, CONFIRM_IDENTITY_VERIFICATION, CONFIRMATION} from "../types/pageURL";
+import { BASE_URL, CHECK_YOUR_ANSWERS, CONFIRM_IDENTITY_VERIFICATION, CONFIRMATION } from "../types/pageURL";
 import { USER_DATA } from "../utils/constants";
 import { ClientData } from "../model/ClientData";
 import { Session } from "@companieshouse/node-session-handler";
