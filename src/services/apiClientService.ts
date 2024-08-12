e52b542b6f045688d3819829494ffa5325120faa
sardhani <sardhani@companieshouse.gov.uk>
2024-08-12 16:49:10 +0100

    send verified client details to verification api

--- src/services/apiClientService.ts
+++
@@ -1,7 +0,0 @@
-import { createPrivateApiClient } from "private-api-sdk-node";
-import { CHS_INTERNAL_API_KEY, INTERNAL_API_URL } from "../utils/properties";
-import PrivateApiClient from "private-api-sdk-node/dist/client";
-
-export const createPrivateApiKeyClient = (): PrivateApiClient => {
-    return createPrivateApiClient(CHS_INTERNAL_API_KEY, undefined, INTERNAL_API_URL);
-};
