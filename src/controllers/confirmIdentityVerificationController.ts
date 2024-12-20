682891c87a15b17626e4e391c789d2e9b4d32b22
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-12-20 15:29:47 +0000

    Update personal and preferred name display in confirmation statement

--- src/controllers/confirmIdentityVerificationController.ts
+++ src/controllers/confirmIdentityVerificationController.ts
@@ -43,7 +43,10 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
             formattedDate,
             payload,
             acspName: acspDetails.name,
-            amlBodies
+            amlBodies,
+            publicRegisterName: clientData?.publicRegisterName,
+            publicRegisterLastName: clientData?.publicRegisterLastName,
+            useNameOnPublicRegister: clientData.useNameOnPublicRegister
         });
     } catch (error) {
         logger.error("acsp profile data api error" + JSON.stringify(error));
@@ -75,7 +78,10 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
             formattedDate,
             ...pageProperties,
             amlBodies,
-            acspName: acspDetails.name
+            acspName: acspDetails.name,
+            publicRegisterName: clientData?.publicRegisterName,
+            publicRegisterLastName: clientData?.publicRegisterLastName,
+            useNameOnPublicRegister: clientData.useNameOnPublicRegister
         });
     } else {
         if (clientData) {
