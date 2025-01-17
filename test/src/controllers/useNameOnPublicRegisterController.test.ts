27956df1ff0f6cdc78075f4bcf95a41ef3e951fc
rnesbitt-ch <rnesbitt@companieshouse.gov.uk>
2025-01-17 15:46:40 +0000

    IDVA5-1647-check-your-answers-routing

--- test/src/controllers/useNameOnPublicRegisterController.test.ts
+++ test/src/controllers/useNameOnPublicRegisterController.test.ts
@@ -1,11 +1,15 @@
 import mocks from "../../mocks/all_middleware_mock";
 import supertest from "supertest";
 import app from "../../../src/app";
-import { BASE_URL, USE_NAME_ON_PUBLIC_REGISTER, PERSONAL_CODE, PERSONS_NAME_ON_PUBLIC_REGISTER } from "../../../src/types/pageURL";
-
-jest.mock("../../../src/services/identityVerificationService");
+import { BASE_URL, USE_NAME_ON_PUBLIC_REGISTER, PERSONAL_CODE, PERSONS_NAME_ON_PUBLIC_REGISTER, CHECK_YOUR_ANSWERS } from "../../../src/types/pageURL";
+import { sessionMiddleware } from "../../../src/middleware/session_middleware";
+import { getSessionRequestWithPermission } from "../../mocks/session.mock";
+import { PREVIOUS_PAGE_URL, USER_DATA } from "../../../src/utils/constants";
+import { Request, Response, NextFunction } from "express";
 
 const router = supertest(app);
+
+let customMockSessionMiddleware: any;
 
 describe("GET" + USE_NAME_ON_PUBLIC_REGISTER, () => {
     it("should return status 200", async () => {
@@ -34,4 +38,28 @@ describe("POST" + USE_NAME_ON_PUBLIC_REGISTER, () => {
         expect(res.status).toBe(400);
         expect(res.text).toContain("Select if we can use the name on the public register");
     });
+
+    it("should return status 302 after redirect to Check Your Answers - option 1 selected", async () => {
+        createMockSessionMiddleware();
+        const res = await router.post(BASE_URL + USE_NAME_ON_PUBLIC_REGISTER).send({ useNameOnPublicRegisterRadio: "use_name_on_public_register_yes" });
+        expect(res.status).toBe(302);
+        expect(res.header.location).toBe(BASE_URL + CHECK_YOUR_ANSWERS + "?lang=en");
+    });
 });
+
+function createMockSessionMiddleware () {
+    customMockSessionMiddleware = sessionMiddleware as jest.Mock;
+    const session = getSessionRequestWithPermission();
+    session.setExtraData(PREVIOUS_PAGE_URL, "/tell-companies-house-you-have-verified-someones-identity/check-your-answers?lang=en");
+    session.setExtraData(USER_DATA, {
+        firstName: "John",
+        lastName: "Doe",
+        preferredFirstName: "",
+        preferredMiddleName: "",
+        preferredLastName: ""
+    });
+    customMockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+        req.session = session;
+        next();
+    });
+}
