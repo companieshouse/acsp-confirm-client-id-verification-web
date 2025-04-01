b2fb45d598bdf5d90bd0fc6f2303a5041c1fb10d
rdas <rdas@companieshouse.gov.uk>
2025-04-01 12:04:26 +0100

    IDVA5-1958 Allow BRPs to be used 18 months after expiry date -- renamed the method name, redefined the constant

--- src/validations/idDocumentDetails.ts
+++ src/validations/idDocumentDetails.ts
@@ -99,8 +99,9 @@ export const validateAgainstWhenIdDocsChecked = (day: number, month: number, yea
     const clientData: ClientData = req?.getExtraData(USER_DATA)!;
     const whenIdDocsChecked: Date = new Date(clientData.whenIdentityChecksCompleted!);
     const expiryDate = new Date(year, month - 1, day);
-    if (ID_DOCUMENTS_WITH_GRACED_EXPIRY.has(clientData.documentsChecked?.[docSequence - 1]!)) {
-        ID_DOCUMENTS_WITH_GRACED_EXPIRY.forEach((value, key) => {
+    const documentsWithGracedExpiryMap = new Map(Object.entries(ID_DOCUMENTS_WITH_GRACED_EXPIRY));
+    if (documentsWithGracedExpiryMap.has(clientData.documentsChecked?.[docSequence - 1]!)) {
+        documentsWithGracedExpiryMap.forEach((value, key) => {
             if (clientData.documentsChecked?.[docSequence - 1] === key) {
                 validateExpiredDocuments(value,
                     whenIdDocsChecked,
