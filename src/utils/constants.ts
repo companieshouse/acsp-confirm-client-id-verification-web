cc4b7b5761fea1bcdeaadbf25fd01de7e8cf421c
rnesbitt-ch <rnesbitt@companieshouse.gov.uk>
2025-04-02 09:08:33 +0100

    Adding constants for link ids

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,6 +6,9 @@ export const CHECK_YOUR_ANSWERS_FLAG = "checkYourAnswersFlag";
 export const ACSP_DETAILS = "acspDetails";
 export const BIOMETRIC_PASSPORT = "biometric_passport";
 export const PASSPORT = "passport";
+export const VERIFY_SERVICE_LINK = "verify-service-link";
+export const SERVICE_URL_LINK = "service-url-link";
+export const AUTHORISED_AGENT_ACCOUNT_LINK = "authorised-agent-account-link";
 
 // Matomo
 export const MATOMO_LINK_CLICK = "Click link";
