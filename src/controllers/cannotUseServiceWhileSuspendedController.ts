7974a7a7544b1d1da5696a6d2b3544ce0f32d53f
jmccoubrey-ch <jmccoubrey@companieshouse.gov.uk>
2025-06-23 12:04:32 +0100

    Test changes

--- src/controllers/cannotUseServiceWhileSuspendedController.ts
+++ src/controllers/cannotUseServiceWhileSuspendedController.ts
@@ -7,15 +7,19 @@ import { ACSP_DETAILS } from "../utils/constants";
 import { AUTHORISED_AGENT, BASE_URL, CANNOT_USE_SERVICE_WHILE_SUSPENDED } from "../types/pageURL";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
-    const lang = selectLang(req.query.lang);
-    const locales = getLocalesService();
-    const session: Session = req.session as any as Session;
-    const acspDetails: AcspFullProfile = session.getExtraData(ACSP_DETAILS)!;
+    try {
+        const lang = selectLang(req.query.lang);
+        const locales = getLocalesService();
+        const session: Session = req.session as any as Session;
+        const acspDetails: AcspFullProfile = session.getExtraData(ACSP_DETAILS)!;
 
-    res.render(config.CANNOT_USE_SERVICE_WHILE_SUSPENDED, {
-        ...getLocaleInfo(locales, lang),
-        currentUrl: BASE_URL + CANNOT_USE_SERVICE_WHILE_SUSPENDED,
-        businessName: acspDetails.name,
-        authorisedAgentLink: AUTHORISED_AGENT
-    });
+        res.render(config.CANNOT_USE_SERVICE_WHILE_SUSPENDED, {
+            ...getLocaleInfo(locales, lang),
+            currentUrl: BASE_URL + CANNOT_USE_SERVICE_WHILE_SUSPENDED,
+            businessName: acspDetails.name,
+            authorisedAgentLink: AUTHORISED_AGENT
+        });
+    } catch (error) {
+        next(error);
+    }    
 };
