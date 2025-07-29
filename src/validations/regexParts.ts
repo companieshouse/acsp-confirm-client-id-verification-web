a9d4876362ba7129322d69381e57a561391b054e
jmccoubrey-ch <jmccoubrey@companieshouse.gov.uk>
2025-07-29 16:25:44 +0100

    Include '~'

--- src/validations/regexParts.ts
+++ src/validations/regexParts.ts
@@ -4,4 +4,4 @@ export const NUMBERS = "\\p{N}"; // Any kind of digit
 export const PUNCTUATION = "\\p{P}";
 export const SYMBOLS = "\\p{S}";
 export const WHITESPACE = "\\s";
-export const EXCLUDED_CHARS = "\\^|";
+export const EXCLUDED_CHARS = "\\^|~";
