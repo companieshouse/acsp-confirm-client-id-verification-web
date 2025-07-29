0cb6c9420defc7baac53f9e3a65bf6951f25b8e7
jmccoubrey-ch <jmccoubrey@companieshouse.gov.uk>
2025-07-29 11:34:02 +0100

    Extend address to all special chars

---
+++ src/validations/regexParts.ts
@@ -0,0 +1,7 @@
+// Unicode character classes
+export const LETTERS = "\\p{L}"; // Any kind of letter
+export const NUMBERS = "\\p{N}"; // Any kind of digit
+export const WHITESPACE = "\\s";
+
+// Special characters (escaped where needed)
+export const SPECIAL_CHARS = `\\-'.+<>;:,"!()\\[\\]{}\\\\/%#=*¥€$£@&\`´^~˚ˉ˘˛°çčþđøæœ`;
