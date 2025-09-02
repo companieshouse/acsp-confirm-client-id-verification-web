12e406925b6e7b9f20cd6d42e8c7c4b47ecf63dc
rdas <rdas@companieshouse.gov.uk>
2025-09-02 10:15:58 +0100

    IDVA5-2269 Reverification Service - What is their home address? --added test coverage for the name and back redirection

--- test/src/controllers/reverify-somones-identity/whatIsTheHomeAddress.test.ts
+++ test/src/controllers/reverify-somones-identity/whatIsTheHomeAddress.test.ts
@@ -1,10 +1,18 @@
 import mocks from "../../../mocks/all_middleware_mock";
 import supertest from "supertest";
 import app from "../../../../src/app";
-import { REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS, REVERIFY_BASE_URL, REVERIFY_CHOOSE_AN_ADDRESS, REVERIFY_CONFIRM_HOME_ADDRESS } from "../../../../src/types/pageURL";
+import {
+    REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS,
+    REVERIFY_BASE_URL,
+    REVERIFY_CHOOSE_AN_ADDRESS,
+    REVERIFY_CONFIRM_HOME_ADDRESS,
+    REVERIFY_CHECK_YOUR_ANSWERS,
+    REVERIFY_DATE_OF_BIRTH
+} from "../../../../src/types/pageURL";
 import { getAddressFromPostcode } from "../../../../src/services/postcode-lookup-service";
 import { UKAddress } from "@companieshouse/api-sdk-node/dist/services/postcode-lookup/types";
 import * as localise from "../../../../src/utils/localise";
+import * as urlService from "../../../../src/services/url";
 
 jest.mock("../../../../src/services/postcode-lookup-service.ts");
 
@@ -18,6 +26,14 @@ const mockResponseBodyOfUKAddress: UKAddress[] = [{
 }];
 
 describe("GET" + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS, () => {
+    const addLangToUrl = localise.addLangToUrl;
+    const lang = "en";
+    const checkYourAnswersUrl = addLangToUrl(REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS, lang);
+    const dateOfBirthUrl = addLangToUrl(REVERIFY_BASE_URL + REVERIFY_DATE_OF_BIRTH, lang);
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
     it("should return status 200", async () => {
         const res = await router.get(REVERIFY_BASE_URL + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS);
         expect(res.status).toBe(200);
@@ -35,10 +51,33 @@ describe("GET" + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS, () => {
         expect(res.status).toBe(500);
         expect(res.text).toContain("Sorry we are experiencing technical difficulties");
     });
+
+    it("should use previousPage as check your answers when previousPageUrl matches", async () => {
+        jest.spyOn(urlService, "getPreviousPageUrl").mockReturnValue(checkYourAnswersUrl);
+
+        const res = await router.get(REVERIFY_BASE_URL + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS);
+        expect(res.status).toBe(200);
+        expect(res.text).toContain(REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS + "?lang=en");
+    });
+
+    it("should use previousPage as date of birth when previousPageUrl does not match check your answers", async () => {
+        jest.spyOn(urlService, "getPreviousPageUrl").mockReturnValue(dateOfBirthUrl);
+
+        const res = await router.get(REVERIFY_BASE_URL + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS);
+        expect(res.status).toBe(200);
+        expect(res.text).toContain(REVERIFY_BASE_URL + REVERIFY_DATE_OF_BIRTH + "?lang=en");
+    });
 });
 
 describe("POST" + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS, () => {
+    const addLangToUrl = localise.addLangToUrl;
+    const lang = "en";
+    const checkYourAnswersUrl = addLangToUrl(REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS, lang);
+    const dateOfBirthUrl = addLangToUrl(REVERIFY_BASE_URL + REVERIFY_DATE_OF_BIRTH, lang);
 
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
     it("should redirect to address list with status 302 on successful form submission", async () => {
         const formData = {
             postCode: "ST63LJ",
@@ -119,7 +158,52 @@ describe("POST" + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS, () => {
         expect(res.text).toContain("Doe");
         expect(res.text).toContain("We cannot find this postcode. Enter a different one, or enter the address manually");
     });
+    it("should use previousPage as check your answers when previousPageUrl matches", async () => {
+        jest.spyOn(urlService, "getPreviousPageUrl").mockReturnValue(checkYourAnswersUrl);
 
+        const formData = {
+            postCode: "ST63LJ",
+            premise: ""
+        };
+
+        (getAddressFromPostcode as jest.Mock).mockResolvedValueOnce([{
+            premise: "2",
+            addressLine1: "DUNCALF STREET",
+            postTown: "STOKE-ON-TRENT",
+            postcode: "ST6 3LJ",
+            country: "GB-ENG"
+        }]);
+
+        const res = await router.post(REVERIFY_BASE_URL + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS).send(formData);
+        expect(res.status).toBe(302);
+        (getAddressFromPostcode as jest.Mock).mockRejectedValueOnce(new Error("Postcode not found"));
+        const res2 = await router.post(REVERIFY_BASE_URL + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS).send({ postCode: "AB12CD", premise: "" });
+        expect(res2.status).toBe(400);
+        expect(res2.text).toContain(REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS + "?lang=en");
+    });
+
+    it("should use previousPage as date of birth when previousPageUrl does not match check your answers", async () => {
+        jest.spyOn(urlService, "getPreviousPageUrl").mockReturnValue(dateOfBirthUrl);
+
+        const formData = {
+            postCode: "ST63LJ",
+            premise: ""
+        };
+        (getAddressFromPostcode as jest.Mock).mockResolvedValueOnce([{
+            premise: "2",
+            addressLine1: "DUNCALF STREET",
+            postTown: "STOKE-ON-TRENT",
+            postcode: "ST6 3LJ",
+            country: "GB-ENG"
+        }]);
+
+        const res = await router.post(REVERIFY_BASE_URL + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS).send(formData);
+        expect(res.status).toBe(302);
+        (getAddressFromPostcode as jest.Mock).mockRejectedValueOnce(new Error("Postcode not found"));
+        const res2 = await router.post(REVERIFY_BASE_URL + REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS).send({ postCode: "AB12CD", premise: "" });
+        expect(res2.status).toBe(400);
+        expect(res2.text).toContain(REVERIFY_BASE_URL + REVERIFY_DATE_OF_BIRTH + "?lang=en");
+    });
     it("should show the error page if an error occurs", async () => {
         const errorMessage = "Test error";
         jest.spyOn(localise, "getLocalesService").mockImplementationOnce(() => {
