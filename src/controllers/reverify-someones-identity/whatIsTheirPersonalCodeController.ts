9f19be2c3b8b4c9a54ef16dc374f9bc0c8b854dd
rdas <rdas@companieshouse.gov.uk>
2025-09-08 14:10:39 +0100

    IDVA5-2421 Enter a clients UVID/ Personal code

--- src/controllers/reverify-someones-identity/whatIsTheirPersonalCodeController.ts
+++ src/controllers/reverify-someones-identity/whatIsTheirPersonalCodeController.ts
@@ -2,51 +2,35 @@
 import { NextFunction, Request, Response } from "express";
 import * as config from "../../config";
 import {
-    REVERIFY_WHAT_IS_THEIR_HOME_ADDRESS,
     REVERIFY_DATE_OF_BIRTH,
     REVERIFY_BASE_URL,
     REVERIFY_CHECK_YOUR_ANSWERS,
-    REVERIFY_HOME_ADDRESS_MANUAL,
-    REVERIFY_CONFIRM_HOME_ADDRESS,
-    REVERIFY_CHOOSE_AN_ADDRESS,
-    REVERIFY_PERSONAL_CODE
+    REVERIFY_PERSONAL_CODE,
+    REVERIFY_EMAIL_ADDRESS
 } from "../../types/pageURL";
 import { addLangToUrl, getLocaleInfo, getLocalesService, selectLang } from "../../utils/localise";
 import { getPreviousPageUrl } from "../../services/url";
 import { saveDataInSession } from "../../utils/sessionHelper";
 import { formatValidationError, getPageProperties } from "../../validations/validation";
-import { ValidationError, validationResult } from "express-validator";
-import { Session } from "@companieshouse/node-session-handler";
-import { ClientData } from "model/ClientData";
-import { AddressLookUpService } from "../../services/addressLookup";
-import { USER_DATA, PREVIOUS_PAGE_URL } from "../../utils/constants";
+import { validationResult } from "express-validator";
+import { PREVIOUS_PAGE_URL } from "../../utils/constants";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const lang = selectLang(req.query.lang);
         const locales = getLocalesService();
-        const session: Session = req.session as any as Session;
-        const clientData: ClientData = session.getExtraData(USER_DATA) ? session.getExtraData(USER_DATA)! : {};
-
-        const payload = {
-            postCode: clientData.address?.postcode,
-            premise: clientData.address?.propertyDetails
-        };
 
         const previousPageUrl = getPreviousPageUrl(req, REVERIFY_BASE_URL);
         saveDataInSession(req, PREVIOUS_PAGE_URL, previousPageUrl);
 
-        const previousPage = previousPageUrl === addLangToUrl(REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS, lang)
+        const previousPage = previousPageUrl === addLangToUrl(REVERIFY_BASE_URL, lang)
             ? addLangToUrl(REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS, lang)
-            : addLangToUrl(REVERIFY_BASE_URL + REVERIFY_DATE_OF_BIRTH, lang);
+            : addLangToUrl(REVERIFY_BASE_URL, lang);
 
         res.render(config.REVERIFY_PERSONAL_CODE, {
             ...getLocaleInfo(locales, lang),
             previousPage: previousPage,
-            currentUrl: REVERIFY_BASE_URL + REVERIFY_PERSONAL_CODE,
-            payload,
-            firstName: clientData.firstName,
-            lastName: clientData.lastName
+            currentUrl: REVERIFY_BASE_URL + REVERIFY_PERSONAL_CODE
         });
     } catch (error) {
         next(error);
@@ -59,17 +43,17 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
         const lang = selectLang(req.query.lang);
         const locales = getLocalesService();
         const errorList = validationResult(req);
-        const session: Session = req.session as any as Session;
-        const clientData: ClientData = session?.getExtraData(USER_DATA)!;
         const currentUrl = REVERIFY_BASE_URL + REVERIFY_PERSONAL_CODE;
-
         const previousPageUrl = getPreviousPageUrl(req, REVERIFY_DATE_OF_BIRTH);
+
         saveDataInSession(req, PREVIOUS_PAGE_URL, previousPageUrl);
+
         const previousPage = previousPageUrl === addLangToUrl(REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS, lang)
             ? addLangToUrl(REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS, lang)
             : addLangToUrl(REVERIFY_BASE_URL + REVERIFY_DATE_OF_BIRTH, lang);
 
         if (errorList.isEmpty()) {
+            res.redirect(addLangToUrl(REVERIFY_BASE_URL + REVERIFY_EMAIL_ADDRESS, lang));
         } else {
             const pageProperties = getPageProperties(formatValidationError(errorList.array(), lang));
             res.status(400).render(config.REVERIFY_PERSONAL_CODE, {
@@ -77,8 +61,6 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
                 previousPage,
                 currentUrl,
                 payload: req.body,
-                firstName: clientData?.firstName,
-                lastName: clientData?.lastName,
                 ...pageProperties
             });
         }
--- src/validations/personalCode.ts
+++ src/validations/personalCode.ts
@@ -1,9 +1,8 @@
 import { body } from "express-validator";
-import { EXCLUDED_CHARS, LETTERS, NUMBERS, PUNCTUATION, SYMBOLS, WHITESPACE } from "./regexParts";
 
-const extendedAddressDetailsPattern = `^(?!.*[${EXCLUDED_CHARS}])[${LETTERS}${NUMBERS}${PUNCTUATION}${SYMBOLS}${WHITESPACE}]*$`;
+const extendedAddressDetailsPattern = /^[A-Z0-9-]{13}$/;
 const extendedAddressDetailsFormat: RegExp = new RegExp(extendedAddressDetailsPattern, "u");
+
 export const personalCodeValidator = [
-    body("personalCode").trim().notEmpty().withMessage("noPersonalCode").bail(),
-    body("personalCode").trim().matches(extendedAddressDetailsFormat).withMessage("personalCodeInvalidFormat").bail()
+    body("personalCode").trim().notEmpty().withMessage("noPersonalCode").bail().matches(extendedAddressDetailsFormat).withMessage("personalCodeInvalidFormat")
 ];
