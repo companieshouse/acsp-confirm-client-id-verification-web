1f876016ba44d27f9ec606d581dc468dce6f3c0a
cbroadley-ch <cbroadley@companieshouse.gov.uk>
2025-09-12 12:15:01 +0100

    Increasing test coverage

--- test/src/controllers/reverify-somones-identity/howIdentityDocsWereCheckedController.test.ts
+++ test/src/controllers/reverify-somones-identity/howIdentityDocsWereCheckedController.test.ts
@@ -1,12 +1,21 @@
 import mocks from "../../../mocks/all_middleware_mock";
 import supertest from "supertest";
 import app from "../../../../src/app";
-import { REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED, REVERIFY_BASE_URL, REVERIFY_WHICH_IDENTITY_DOCS_CHECKED_GROUP2, REVERIFY_WHICH_IDENTITY_DOCS_CHECKED_GROUP1, REVERIFY_CHECK_YOUR_ANSWERS } from "../../../../src/types/pageURL";
+import { REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED, REVERIFY_BASE_URL, REVERIFY_WHICH_IDENTITY_DOCS_CHECKED_GROUP2, REVERIFY_WHICH_IDENTITY_DOCS_CHECKED_GROUP1, REVERIFY_CHECK_YOUR_ANSWERS, REVERIFY_WHEN_IDENTITY_CHECKS_COMPLETED } from "../../../../src/types/pageURL";
 import * as localise from "../../../../src/utils/localise";
-import { CRYPTOGRAPHIC_SECURITY_FEATURES, PHYSICAL_SECURITY_FEATURES } from "../../../../src/utils/constants";
+import { CRYPTOGRAPHIC_SECURITY_FEATURES, PHYSICAL_SECURITY_FEATURES, USER_DATA, PREVIOUS_PAGE_URL } from "../../../../src/utils/constants";
+import { sessionMiddleware } from "../../../../src/middleware/session_middleware";
+import { getSessionRequestWithPermission } from "../../../mocks/session.mock";
+import { Request, Response, NextFunction } from "express";
 
 jest.mock("@companieshouse/api-sdk-node");
 const router = supertest(app);
+
+let customMockSessionMiddleware: any;
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
 
 describe("GET" + REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED, () => {
 
@@ -33,6 +42,61 @@ describe("GET" + REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED, () => {
             .set("Referer", REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS + "?lang=en");
         expect(res.status).toBe(200);
         expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
+        expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+    });
+
+    it("should return status 200 when no user data exists in session", async () => {
+        customMockSessionMiddleware = sessionMiddleware as jest.Mock;
+        const session = getSessionRequestWithPermission();
+
+        session.setExtraData(USER_DATA, undefined);
+        customMockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+            req.session = session;
+            next();
+        });
+
+        const res = await router.get(REVERIFY_BASE_URL + REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED);
+        expect(res.status).toBe(200);
+        expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+    });
+
+    it("should return status 200 when user data with firstName and lastName exists in session", async () => {
+        customMockSessionMiddleware = sessionMiddleware as jest.Mock;
+        const session = getSessionRequestWithPermission();
+
+        session.setExtraData(USER_DATA, {
+            firstName: "Test",
+            lastName: "User",
+            howIdentityDocsChecked: CRYPTOGRAPHIC_SECURITY_FEATURES
+        });
+        customMockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+            req.session = session;
+            next();
+        });
+
+        const res = await router.get(REVERIFY_BASE_URL + REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED);
+        expect(res.status).toBe(200);
+        expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+        expect(res.text).toContain("Test");
+        expect(res.text).toContain("User");
+    });
+
+    it("should return status 200 when accessing from check your answers URL with previous page URL set", async () => {
+        customMockSessionMiddleware = sessionMiddleware as jest.Mock;
+        const session = getSessionRequestWithPermission();
+        session.setExtraData(PREVIOUS_PAGE_URL, REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS + "?lang=en");
+        session.setExtraData(USER_DATA, {
+            firstName: "Test",
+            lastName: "User"
+        });
+
+        customMockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+            req.session = session;
+            next();
+        });
+
+        const res = await router.get(REVERIFY_BASE_URL + REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED);
+        expect(res.status).toBe(200);
         expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
     });
 });
@@ -64,5 +128,47 @@ describe("POST" + REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED, () => {
         const res = await router.post(REVERIFY_BASE_URL + REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED);
         expect(res.status).toBe(500);
         expect(res.text).toContain("Sorry we are experiencing technical difficulties");
+    });
+
+    it("should return status 400 with client data firstName and lastName when validation fails", async () => {
+        customMockSessionMiddleware = sessionMiddleware as jest.Mock;
+        const session = getSessionRequestWithPermission();
+
+        session.setExtraData(USER_DATA, {
+            firstName: "Test",
+            lastName: "User"
+        });
+        session.setExtraData(PREVIOUS_PAGE_URL, REVERIFY_BASE_URL + REVERIFY_WHEN_IDENTITY_CHECKS_COMPLETED + "?lang=en");
+
+        customMockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+            req.session = session;
+            next();
+        });
+
+        const res = await router.post(REVERIFY_BASE_URL + REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED).send({ howIdentityDocsCheckedRadio: "" });
+        expect(res.status).toBe(400);
+        expect(res.text).toContain("Select if the identity documents were checked using technology or by a person");
+        expect(res.text).toContain("Test");
+        expect(res.text).toContain("User");
+    });
+
+    it("should return status 400 when validation fails and previousPageUrl is check your answers", async () => {
+        customMockSessionMiddleware = sessionMiddleware as jest.Mock;
+        const session = getSessionRequestWithPermission();
+
+        session.setExtraData(USER_DATA, {
+            firstName: "Test",
+            lastName: "User"
+        });
+        session.setExtraData(PREVIOUS_PAGE_URL, REVERIFY_BASE_URL + REVERIFY_CHECK_YOUR_ANSWERS + "?lang=en");
+
+        customMockSessionMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => {
+            req.session = session;
+            next();
+        });
+
+        const res = await router.post(REVERIFY_BASE_URL + REVERIFY_HOW_IDENTITY_DOCUMENTS_CHECKED).send({ howIdentityDocsCheckedRadio: "" });
+        expect(res.status).toBe(400);
+        expect(res.text).toContain("Select if the identity documents were checked using technology or by a person");
     });
 });
