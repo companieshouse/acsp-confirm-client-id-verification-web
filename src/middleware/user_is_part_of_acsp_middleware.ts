fef40ac3bf4be8ad66b0629819ee516462301c3b
mreid-ch <mreid@companieshouse.gov.uk>
2025-09-18 10:24:00 +0100

    IDVA5-2550-creating-kickout-screen-when-user-is-not-ACSP

--- src/middleware/user_is_part_of_acsp_middleware.ts
+++ src/middleware/user_is_part_of_acsp_middleware.ts
@@ -1,19 +1,20 @@
 import { NextFunction, Request, Response } from "express";
-import { BASE_URL, MUST_BE_AUTHORISED_AGENT } from "../types/pageURL";
+import { BASE_URL, MUST_BE_AUTHORISED_AGENT, REVERIFY_BASE_URL, REVERIFY_MUST_BE_AUTHORISED_AGENT } from "../types/pageURL";
 import { addLangToUrl, selectLang } from "../utils/localise";
 import { getLoggedInAcspNumber } from "../utils/session";
-
 export const userIsPartOfAcspMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const lang = selectLang(req.query.lang);
         const acspNumber: string = getLoggedInAcspNumber(req.session);
+        const isReverifyService: boolean = req.originalUrl.includes(REVERIFY_BASE_URL);
         if (!acspNumber) {
-            if (!req.originalUrl.includes(MUST_BE_AUTHORISED_AGENT)) {
-                res.redirect(addLangToUrl(BASE_URL + MUST_BE_AUTHORISED_AGENT, lang));
+            const mustBeAuthorisedAgentUrl = isReverifyService ? REVERIFY_MUST_BE_AUTHORISED_AGENT : MUST_BE_AUTHORISED_AGENT;
+            const baseUrl = isReverifyService ? REVERIFY_BASE_URL : BASE_URL;
+            if (!req.originalUrl.includes(mustBeAuthorisedAgentUrl)) {
+                res.redirect(addLangToUrl(baseUrl + mustBeAuthorisedAgentUrl, lang));
                 return;
             }
         }
-
         next();
     } catch (error) {
         next(error);
