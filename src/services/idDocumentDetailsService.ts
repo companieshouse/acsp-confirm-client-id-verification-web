0f1b70889c45a5baa31b69c95372226038926ce5
hepsimo <sboyd@companieshouse.gov.uk>
2026-02-27 07:50:47 +0000

    Align optional country logic with displayed hints
    - revert getErrorForSpecificDocs logic to match live release
    - add docs to optional country set to match that logic

--- src/services/idDocumentDetailsService.ts
+++ src/services/idDocumentDetailsService.ts
@@ -69,15 +69,11 @@ const getErrorForSpecificDocs = (docName: string, errorMessage: string, errorTex
         documentsWithGracedExpiryMap = new Map(Object.entries(OPTION_2_ID_DOCUMENTS_WITH_GRACED_EXPIRY));
     }
 
-    const optionalExpiryDateDocs = [i18n.UK_PASS_card, i18n.UK_HM_veteran_card, i18n.PRADO_supported_photo_id, i18n.work_permit_photo_id];
-    const optionalCountryOfIssueDocs = [i18n.UK_or_EU_digital_tachograph_card, i18n.PRADO_supported_photo_id];
-    const optionalNumberDocs = [i18n.PRADO_supported_photo_id, i18n.work_permit_photo_id];
-
-    if ((optionalExpiryDateDocs.includes(docName) && errorText === "noExpiryDate") ||
-        (optionalCountryOfIssueDocs.includes(docName) && errorText === "noCountry") ||
-        (optionalNumberDocs.includes(docName) && errorText === "docNumberInput")) {
-        return ""; // do not display error message for optional fields for specific docs
-    // replace date placeholder with formatted date if error is about invalid expirydate
+    if (((docName === i18n.UK_PASS_card || docName === i18n.UK_HM_veteran_card) && errorText === "noExpiryDate") ||
+        ((docName === i18n.PRADO_supported_photo_id || docName === i18n.work_permit_photo_id) &&
+         (errorText === "noExpiryDate" || errorText === "noCountry" || errorText === "docNumberInput")) ||
+        (docName === i18n.UK_or_EU_digital_tachograph_card && errorText === "noCountry")) {
+        return "";
     } else if (errorText === "dateAfterIdChecksDone" ||
         documentsWithGracedExpiryMap.has(errorText)) {
         const idChecksCompletedDate = FormatService.formatDateForLocale(whenIdDocsChecked, lang);
